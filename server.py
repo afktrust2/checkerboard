eb7e168f425e9692e4c85b15b6cb394e23c50681
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2022-03-04 01:24:12 -0800

    fixed colors

--- server.py
+++ server.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("checkerboard.html",row=8,col=8)
+    return render_template("checkerboard.html",row=8,col=8,color1='red',color2='black')
 
 @app.route('/<int:x>')
 def row(x):
-    return render_template("checkerboard.html",row=x,col=8)
+    return render_template("checkerboard.html",row=x,col=8,color1='red',color2='black')
 
 @app.route('/<int:x>/<int:y>')
 def row_col(x,y):
-    return render_template("checkerboard.html",row=x,col=y)
+    return render_template("checkerboard.html",row=x,col=y,color1='red',color2='black')
 
 @app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
 def four(x,y,color1,color2):
